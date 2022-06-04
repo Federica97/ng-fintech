22f7d0a8fa52650ba8052025512cd3535792944a
Federica97 <34345538+Federica97@users.noreply.github.com>
2022-06-05 00:34:25 +0200

    Update navigation-menu.module.ts

--- src/app/shared/modules/navigation-menu.module.ts
+++ src/app/shared/modules/navigation-menu.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavigationMenuComponent } from './navigation-menu.component';
+import {MaterialModule} from "../material/material.module";
+import {RouterModule} from "@angular/router";
+import {CoreModule} from "../../core/core.module";
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavigationMenuComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    RouterModule,
+    CoreModule
+  ],
+  exports: [
+    NavigationMenuComponent
   ]
 })
 export class NavigationMenuModule { }
