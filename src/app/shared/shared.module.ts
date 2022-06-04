779c2b18b531aaaf7077d467cb6a88bb76fc57b1
Federica97 <34345538+Federica97@users.noreply.github.com>
2022-06-05 00:35:15 +0200

    Update shared.module.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SnackbarComponent } from './components/snackbar.component';
+import {MaterialModule} from "./material/material.module";
+import { TruncatePipe } from './pipes/truncate.pipe';
+import { FilterByTextPipe } from '../views/transfer/pipes/filter-by-text.pipe';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SnackbarComponent,
+    TruncatePipe
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
+  ],
+  exports: [
+    SnackbarComponent,
+    TruncatePipe
   ]
 })
 export class SharedModule { }
