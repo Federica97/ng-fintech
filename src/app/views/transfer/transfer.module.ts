04d4619ac4d8a1ecc39c0e00e69b9227660f14c2
Federica97 <34345538+Federica97@users.noreply.github.com>
2022-06-05 00:50:05 +0200

    Update transfer.module.ts

--- src/app/views/transfer/transfer.module.ts
+++ src/app/views/transfer/transfer.module.ts
@@ -1,17 +1,43 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { TransferRoutingModule } from './transfer-routing.module';
-import { TransferComponent } from './transfer.component';
+import {TransferRoutingModule} from './transfer-routing.module';
+import {TransferComponent} from './transfer.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MaterialModule} from "../../shared/material/material.module";
+import {ContactsComponent} from './components/contacts.component';
+import {ContactListComponent} from "./components/contact-list.component";
+import {DeleteContactModalComponent} from "./components/delete-contact-dialog.component";
+import {FilterByTextPipe} from "./pipes/filter-by-text.pipe";
+import {ContactFormComponent} from './components/contact-form.component';
+import {TransferService} from "../../api/transfer.service";
+import {StoreModule} from "@ngrx/store";
+import {EffectsModule} from "@ngrx/effects";
+import {contactsFeature} from "./store/contacts/contacts.reducer";
+import {ContactsEffects} from "./store/contacts/contacts.effects";
+import {HttpClientModule} from "@angular/common/http";
+import {CardsService} from "../../api/cards.service";
 
 
 @NgModule({
   declarations: [
-    TransferComponent
+    TransferComponent,
+    ContactsComponent,
+    ContactListComponent,
+    DeleteContactModalComponent,
+    FilterByTextPipe,
+    ContactFormComponent
   ],
   imports: [
     CommonModule,
-    TransferRoutingModule
-  ]
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    MaterialModule,
+    TransferRoutingModule,
+    StoreModule.forFeature(contactsFeature),
+    EffectsModule.forFeature([ContactsEffects])
+  ],
+  providers:[TransferService, CardsService]
 })
 export class TransferModule { }
