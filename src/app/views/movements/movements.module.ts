178d9e58abfde0477899dac3bd8d28de20ff5d37
Federica97 <34345538+Federica97@users.noreply.github.com>
2022-06-05 00:48:46 +0200

    Update movements.module.ts

--- src/app/views/movements/movements.module.ts
+++ src/app/views/movements/movements.module.ts
@@ -1,17 +1,29 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { MovementsRoutingModule } from './movements-routing.module';
-import { MovementsComponent } from './movements.component';
+import {MovementsRoutingModule} from './movements-routing.module';
+import {MovementsComponent} from './movements.component';
+import {MovementComponent} from './components/movement.component';
+import {MaterialModule} from "../../shared/material/material.module";
+import {SharedModule} from "../../shared/shared.module";
+import {MovementsService} from "../../api/movements.service";
+import {HttpClientModule} from "@angular/common/http";
+import {CardsService} from "../../api/cards.service";
 
 
 @NgModule({
   declarations: [
-    MovementsComponent
+    MovementsComponent,
+    MovementComponent
   ],
   imports: [
     CommonModule,
-    MovementsRoutingModule
-  ]
+    MovementsRoutingModule,
+    HttpClientModule,
+    MaterialModule,
+    SharedModule
+  ],
+  providers: [MovementsService, CardsService]
 })
-export class MovementsModule { }
+export class MovementsModule {
+}
