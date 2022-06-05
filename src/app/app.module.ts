18c6c88e7ad8412c5146cd4ba0a8624d44651a3b
Federica97 <34345538+Federica97@users.noreply.github.com>
2022-06-05 15:25:19 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,15 @@
 import {LOCALE_ID, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialModule} from "./shared/material/material.module";
-
-
 import localeIt from "@angular/common/locales/it";
 import {registerLocaleData} from "@angular/common";
 import {NavigationMenuModule} from "./shared/modules/navigation-menu.module";
 import {DashboardModule} from "./views/dashboard/dashboard.module";
 import {EffectsModule} from "@ngrx/effects";
-import {RouterEffects} from "./store/router/router.effects";
 import {StoreModule} from "@ngrx/store";
 import {StoreDevtoolsModule} from "@ngrx/store-devtools";
 registerLocaleData(localeIt)
